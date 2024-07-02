a71a213e5095e7489c5e931b2b484ecec2785e77
theendercore <theendercore@gmail.com>
2024-07-02 16:21:07 +0300

    - this.getAttackDamage() is gone now using stack.getDamage()

--- src/main/java/net/leah/makeitgoth/item/custom/ChipItem.java
+++ src/main/java/net/leah/makeitgoth/item/custom/ChipItem.java
@@ -1,26 +1,24 @@
 package net.leah.makeitgoth.item.custom;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 
 
 public class ChipItem extends SwordItem {
-        public ChipItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
-            super(toolMaterial, attackDamage, attackSpeed, settings);
-        }
+    public ChipItem(ToolMaterial toolMaterial, Settings settings) {
+        super(toolMaterial, settings);
+    }
 
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         //chip Damage(did not add a damage type just ignores armor)
-        float Damage;
-        Damage=(target.getHealth()-(((float) target.getArmor() /2f)*25f/*percentage of armor to ignore*/)*this.getAttackDamage());
-        target.setHealth(target.getHealth()-Damage);/*There is probably a better way of doing this but I'm lazy*/
+        float Damage = (target.getHealth() - (((float) target.getArmor() / 2f) * 25f/*percentage of armor to ignore*/) * stack.getDamage());
+        target.setHealth(target.getHealth() - Damage);/*There is probably a better way of doing this but I'm lazy*/
+        // yes there is a better way if doing this, a lot better way called target.damage(..)
+        // just add a damage type
+        // if you need a reference ThShipPost mod adds a custom damage type
         return super.postHit(stack, target, attacker);
     }
 }
-
-
-
