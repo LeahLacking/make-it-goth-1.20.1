7c4bd17a003c226cb3381a6c9a98e2a88bc54a7e
theendercore <theendercore@gmail.com>
2024-07-02 16:09:42 +0300

    - removed whitespace - StatusEffects are better registered as Reference

--- src/main/java/net/leah/makeitgoth/effect/ModEffects.java
+++ src/main/java/net/leah/makeitgoth/effect/ModEffects.java
@@ -7,19 +7,18 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 
 import static net.leah.makeitgoth.MakeItGoth.id;
 
 public class ModEffects extends StatusEffects {
 
-    public final static StatusEffect BLEED = registerModEffect("bleed",
-            (new BleedEffect(StatusEffectCategory.HARMFUL, 4718584)));
+    public final static RegistryEntry.Reference<StatusEffect> BLEED = registerModEffect("bleed",
+            new BleedEffect(StatusEffectCategory.HARMFUL, 4718584)
+    );
 
-
-    public static StatusEffect registerModEffect(String name, StatusEffect entry) {
-        return Registry.register(Registries.STATUS_EFFECT, id(name), entry);
-
-
+    public static RegistryEntry.Reference<StatusEffect> registerModEffect(String name, StatusEffect entry) {
+        return Registry.registerReference(Registries.STATUS_EFFECT, id(name), entry);
     }
 
     public static void registerModEffects() {
--- src/main/java/net/leah/makeitgoth/item/custom/BleedItem.java
+++ src/main/java/net/leah/makeitgoth/item/custom/BleedItem.java
@@ -8,17 +8,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 
 public class BleedItem extends AxeItem {
-    public BleedItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
-        super(toolMaterial, attackDamage, attackSpeed, settings);
+    public BleedItem(ToolMaterial toolMaterial, Settings settings) {
+        super(toolMaterial, settings);
     }
 
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         //bleeding
-        target.addStatusEffect(new StatusEffectInstance(ModEffects.BLEED,150,1));
+        target.addStatusEffect(new StatusEffectInstance(ModEffects.BLEED, 150, 1));
         return super.postHit(stack, target, attacker);
-
-
-
     }
 }
