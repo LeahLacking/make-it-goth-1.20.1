ed317472b515d47c2a1e8957cbfdcd380f1148aa
theendercore <theendercore@gmail.com>
2024-07-02 17:32:39 +0300

    - registered tag generators

--- src/main/java/net/leah/makeitgoth/MakeItGothDataGenerator.java
+++ src/main/java/net/leah/makeitgoth/MakeItGothDataGenerator.java
@@ -2,14 +2,26 @@ package net.leah.makeitgoth;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.leah.makeitgoth.datagen.ModBlockTagProvider;
+import net.leah.makeitgoth.datagen.ModItemTagProvider;
 import net.leah.makeitgoth.datagen.ModModelProvider;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MakeItGothDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-		pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModModelProvider::new);
 
-	}
+        // this looks complicated, but is here for laziness’s sake
+        AtomicReference<ModBlockTagProvider> blockTags = new AtomicReference<>();
+        pack.addProvider(((output, reg) -> {
+            blockTags.set(new ModBlockTagProvider(output, reg));
+            return blockTags.get();
+        }));
+        pack.addProvider(((output, reg) -> new ModItemTagProvider(output, reg, blockTags.get())));
+
+    }
 }
