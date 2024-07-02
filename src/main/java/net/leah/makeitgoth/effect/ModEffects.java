06c0645d04d3703a0c68cd3710165a61e24bf6f7
theendercore <theendercore@gmail.com>
2024-07-02 18:06:55 +0300

    - This should not inherit from StatusEffects !

--- src/main/java/net/leah/makeitgoth/effect/ModEffects.java
+++ src/main/java/net/leah/makeitgoth/effect/ModEffects.java
@@ -4,14 +4,13 @@ import net.leah.makeitgoth.MakeItGoth;
 import net.leah.makeitgoth.effect.custom.BleedEffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.entry.RegistryEntry;
 
 import static net.leah.makeitgoth.MakeItGoth.id;
 
-public class ModEffects extends StatusEffects {
+public class ModEffects {
 
     public final static RegistryEntry.Reference<StatusEffect> BLEED = registerModEffect("bleed",
             new BleedEffect(StatusEffectCategory.HARMFUL, 4718584)
