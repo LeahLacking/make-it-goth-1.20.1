a2c8d169100b16094755b42bbfe76dba7bf8228c
theendercore <theendercore@gmail.com>
2024-07-02 17:33:00 +0300

    - added most block tags

--- src/main/java/net/leah/makeitgoth/datagen/ModBlockTagProvider.java
+++ src/main/java/net/leah/makeitgoth/datagen/ModBlockTagProvider.java
@@ -2,11 +2,13 @@ package net.leah.makeitgoth.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.leah.makeitgoth.block.ModBlocks;
+import net.leah.makeitgoth.data.ModTags;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
+
+import static net.leah.makeitgoth.block.ModBlocks.*;
 
 public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
     public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
@@ -16,22 +18,20 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
-                .add(ModBlocks.GOTHSTONE_BRICKS)
-                .add(ModBlocks.GOTHSTONE_BRICK_STAIRS)
-                .add(ModBlocks.GOTHSTONE_BRICK_SLAB)
-                .add(ModBlocks.GOTHSTONE_BRICK_WALL)
-                .add(ModBlocks.GOTHSTONE)
-                .add(ModBlocks.GOTHSTONE_STAIRS)
-                .add(ModBlocks.GOTHSTONE_SLAB)
-                .add(ModBlocks.GOTHSTONE_WALL);
-
-
+                .add(
+                        GOTHSTONE_BRICKS,
+                        GOTHSTONE_BRICK_STAIRS,
+                        GOTHSTONE_BRICK_SLAB,
+                        GOTHSTONE_BRICK_WALL,
+                        GOTHSTONE,
+                        GOTHSTONE_STAIRS,
+                        GOTHSTONE_SLAB,
+                        GOTHSTONE_WALL
+                );
 
 
         getOrCreateTagBuilder(BlockTags.AXE_MINEABLE);
         //all blocks mineable by axe
-
-
 
 
         getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL);
@@ -42,31 +42,63 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
 
         getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
-                .add(ModBlocks.GOTHSTONE_BRICKS)
-                .add(ModBlocks.GOTHSTONE_BRICK_STAIRS)
-                .add(ModBlocks.GOTHSTONE_BRICK_SLAB)
-                .add(ModBlocks.GOTHSTONE_BRICK_WALL)
-                .add(ModBlocks.GOTHSTONE)
-                .add(ModBlocks.GOTHSTONE_STAIRS)
-                .add(ModBlocks.GOTHSTONE_SLAB)
-                .add(ModBlocks.GOTHSTONE_WALL);
+                .add(
+                        GOTHSTONE_BRICKS,
+                        GOTHSTONE_BRICK_STAIRS,
+                        GOTHSTONE_BRICK_SLAB,
+                        GOTHSTONE_BRICK_WALL,
+                        GOTHSTONE,
+                        GOTHSTONE_STAIRS,
+                        GOTHSTONE_SLAB,
+                        GOTHSTONE_WALL
+                );
 
 
+        addRotwoodTags();
+        addGothstoneTags();
+    }
 
 
-        getOrCreateTagBuilder(BlockTags.FENCES)
-         .add(ModBlocks.ROTWOOD_FENCE);
-
+    private void addRotwoodTags() {
+        // (ender) time for wood fun
+        getOrCreateTagBuilder(BlockTags.WOODEN_FENCES)
+                .add(ROTWOOD_FENCE);
 
         getOrCreateTagBuilder(BlockTags.FENCE_GATES)
-        .add(ModBlocks.ROTWOOD_FENCE_GATE);
+                .add(ROTWOOD_FENCE_GATE);
 
+        getOrCreateTagBuilder(BlockTags.WOODEN_PRESSURE_PLATES)
+                .add(ROTWOOD_PRESSURE_PLATE);
+
+        getOrCreateTagBuilder(BlockTags.WOODEN_BUTTONS)
+                .add(ROTWOOD_BUTTON);
+
+        getOrCreateTagBuilder(BlockTags.WOODEN_SLABS)
+                .add(ROTWOOD_SLAB);
+
+        getOrCreateTagBuilder(BlockTags.WOODEN_STAIRS)
+                .add(ROTWOOD_STAIRS);
+
+        getOrCreateTagBuilder(BlockTags.PLANKS)
+                .add(ROTWOOD_PLANKS);
+
+        //logs
+        getOrCreateTagBuilder(ModTags.ROTWOOD_LOGS)
+                .add(ROTWOOD_LOG, STRIPPED_ROTWOOD_LOG, ROTWOOD_WOOD, STRIPPED_ROTWOOD_WOOD);
+        getOrCreateTagBuilder(BlockTags.LOGS_THAT_BURN)
+                .forceAddTag(ModTags.ROTWOOD_LOGS);
+
+    }
+
+    private void addGothstoneTags() {
         getOrCreateTagBuilder(BlockTags.WALLS)
-                .add(ModBlocks.GOTHSTONE_BRICK_WALL)
-                .add(ModBlocks.GOTHSTONE_WALL);
+                .add(GOTHSTONE_BRICK_WALL, GOTHSTONE_WALL);
 
+        getOrCreateTagBuilder(BlockTags.STAIRS)
+                .add(GOTHSTONE_BRICK_STAIRS, GOTHSTONE_STAIRS);
 
-
+        getOrCreateTagBuilder(BlockTags.SLABS)
+                .add(GOTHSTONE_BRICK_SLAB, GOTHSTONE_SLAB);
     }
 }
 
