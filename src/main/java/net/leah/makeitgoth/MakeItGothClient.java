1fe4e51a9c498e1df32e8ac4c42cde9595c204d2
theendercore <theendercore@gmail.com>
2024-10-02 16:45:37 +0300

    rewrote render layer registry

--- src/main/java/net/leah/makeitgoth/MakeItGothClient.java
+++ src/main/java/net/leah/makeitgoth/MakeItGothClient.java
@@ -1,25 +1,21 @@
 package net.leah.makeitgoth;
 
-import com.jcraft.jorbis.Block;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.leah.makeitgoth.block.ModBlocks;
 import net.leah.makeitgoth.screen.BeerBrewingScreen;
 import net.leah.makeitgoth.screen.ModScreenHandlers;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
 
+import java.util.stream.Stream;
+
+import static net.leah.makeitgoth.block.ModBlocks.*;
+
 public class MakeItGothClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.STEEL_GRATE, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.STEEL_BARS, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TEST_BARS, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.EMPTY_BEER_CUP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PUMPKIN_RUM, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BEER,RenderLayer.getCutout());
-
+        Stream.of(STEEL_GRATE, STEEL_BARS, EMPTY_BEER_CUP, PUMPKIN_RUM, BEER)
+                .forEach(block -> BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout()));
 
         HandledScreens.register(ModScreenHandlers.BEER_BREWING_STAND_SCREEN_HANDLER, BeerBrewingScreen::new);
     }
